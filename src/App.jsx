6542f22af6ca2db2a480fd0b7d7f507335ae45a8
CrimvaelVR <maikelvr2004@gmail.com>
2023-10-20 15:29:26 -0400

    Ocultar Header y Hero por el boton de Inventario

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
-import { useState } from 'react'
-import Hero from './components/Hero'
-import HeaderPrueba from './components/Header/HeaderPrueba'
-import Footer from './components/Footer'
-import ProductosL from './components/ProductosL'
-import './App.css'
+
+import { useState } from 'react';
+import Hero from './components/Hero';
+import HeaderPrueba from './components/Header/HeaderPrueba';
+import Footer from './components/Footer';
+import ProductosL from './components/ProductosL';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-    <>
-    <HeaderPrueba></HeaderPrueba>
-    <Hero></Hero>
-
-    <ProductosL></ProductosL>
-    <Footer></Footer>
-    
-    </>
-  )
+    const [showHero, setShowHero] = useState(true);
+    const [showProductos, setShowProductos] = useState(true);
+  
+    const toggleVisibility = () => {
+      setShowHero(false);
+      setShowProductos(false);
+    };
+  
+    return (
+      <>
+        <HeaderPrueba toggleVisibility={toggleVisibility} />
+        {showHero && <Hero />}
+        {showProductos && <ProductosL />}
+        <Footer />
+      </>
+  );
 }
 
-export default App
+export default App;
--- src/components/Header/Menu.jsx
+++ src/components/Header/Menu.jsx
@@ -1,32 +1,35 @@
 import React from 'react';
- 
-const Menu = () => {
-  return <div className="px-2 md:px-0 py-3 space-y-2 md:space-y-0 md:space-x-2 font-medium text-slate-700">
-    <a
-      href="#"
-      className="block md:inline-block px-3 py-2 rounded-md text-white bg-verdeo focus:outline-none focus:text-white focus:bg-verdeo"
-    >
-      Inicio
-    </a>
-    <a
-      href="#"
-      className="block md:inline-block px-3 py-2 rounded-md hover:text-white hover:bg-rojo focus:outline-none focus:text-white focus:bg-rojo"
-    >
-      Inventario
-    </a>
-    <a
-      href="#"
-      className="block md:inline-block px-3 py-2 rounded-md hover:text-white hover:bg-rojo focus:outline-none focus:text-white focus:bg-rojo"
-    >
-      Crear
-    </a>
-    <a
-      href="#"
-      className="block md:inline-block px-3 py-2 rounded-md hover:text-white hover:bg-rojo focus:outline-none focus:text-white focus:bg-rojo"
-    >
-      Contacto
-    </a>
-  </div>
-}
- 
+
+const Menu = (props) => {
+  return (
+    <div className="px-2 md:px-0 py-3 space-y-2 md:space-y-0 md:space-x-2 font-medium text-slate-700">
+      <a
+        href="#"
+        className="block md:inline-block px-3 py-2 rounded-md text-white bg-verdeo focus:outline-none focus:text-white focus:bg-verdeo"
+      >
+        Inicio
+      </a>
+      <a
+        href="#"
+        className="block md:inline-block px-3 py-2 rounded-md hover:text-white hover:bg-rojo focus:outline-none focus:text-white focus:bg-rojo"
+        onClick={props.toggleHeroVisibility}
+      >
+        Inventario
+      </a>
+      <a
+        href="#"
+        className="block md:inline-block px-3 py-2 rounded-md hover:text-white hover:bg-rojo focus:outline-none focus:text-white focus:bg-rojo"
+      >
+        Crear
+      </a>
+      <a
+        href="#"
+        className="block md:inline-block px-3 py-2 rounded-md hover:text-white hover:bg-rojo focus:outline-none focus:text-white focus:bg-rojo"
+      >
+        Contacto
+      </a>
+    </div>
+  );
+};
+
 export default Menu;
