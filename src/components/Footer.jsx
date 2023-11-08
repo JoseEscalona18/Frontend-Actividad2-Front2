82632077b94cb5d46287eae1b125df79bd53b481
CrimvaelVR <maikelvr2004@gmail.com>
2023-11-08 10:39:24 -0400

    Update Hero/Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,26 @@
-import React, { Component } from "react";
+import React, { useContext, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { AuthContext } from "../context/AuthProvider";
+import Loader from "./Header/Loader";
 
-import { Link } from "react-router-dom";
+const Footer = () => {
+  const location = useLocation();
+  const isTokenPresent = document.cookie.includes("token=");
+  const { user } = useContext(AuthContext);
 
-const isTokenPresent = document.cookie.includes('token=');
+  const [loading, setLoading] = useState(false);
 
-export default class Footer extends Component {
-  render() {
-    return (
+  const toggleLoading = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  };
+
+  return (
+    <div >
       <footer className="p-4 bg-verdeo md:p-8 lg:p-10 dark:bg-gray-800 font-[Barlow]">
+
         <div className="mx-auto max-w-screen-xl text-center font-bold">
           {/* Logo */}
           <a
@@ -21,27 +34,52 @@ export default class Footer extends Component {
             Tu tienda favorita en la venta de productos electrónicos
           </p>
           {/* Enlaces */}
-          <ul className="flex flex-wrap justify-center items-center mb-6 text-white space-x-80">
-            {" "}
+          <ul className="flex flex-wrap justify-center items-center mb-6 text-white space-x-6">
             {/* Utilizar la clase space-x-6 */}
             <li>
-              <Link to="/" className="hover:underline hover:text-rojo">
+              <Link to="/" className="hover:underline hover:text-rojo" onClick={toggleLoading}>
                 Inicio
               </Link>{" "}
             </li>
-            <li>
-              <Link
-                to="/Inventario"
-                className="hover:underline hover:text-rojo"
-              >
-                Inventario
-              </Link>
-            </li>
-            <li>
-              <Link to="/Login" className="hover:underline hover:text-rojo">
-                Login
-              </Link>
-            </li>
+            {!isTokenPresent && (
+              <>
+                <li>
+                  <Link
+                    to="/Registro"
+                    className="hover:underline hover:text-rojo"
+                  >
+                    Registro
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/Login" className="hover:underline hover:text-rojo">
+                    Login
+                  </Link>
+                </li>
+              </>
+            )}
+            {isTokenPresent && user?.rol === "admin" && (
+              <>
+                <li>
+                  <Link
+                    to="/Inventario"
+                    className="hover:underline hover:text-rojo"
+                  >
+                    Inventario
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/Admin"
+                    className={`hover:underline hover:text-rojo ${
+                      location.pathname === "/Registro" ? "hidden" : ""
+                    }`}
+                  >
+                    Admin
+                  </Link>
+                </li>
+              </>
+            )}
           </ul>
           {/* Derechos de autor */}
           <span className="text-sm text-white sm:text-center dark:text-gray-400">
@@ -53,6 +91,8 @@ export default class Footer extends Component {
           </span>
         </div>
       </footer>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Footer;
